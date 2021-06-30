b498bd0cad24101888aea21d446cdf2c9a5772d1
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-06-30 19:30:35 -0300

    Realiza exercicio showDogImage

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      foto: '',
+    };
+    this.fetchFunc = this.fetchFunc.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchFunc();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.foto.includes('terrier')) {
+      return false;
+    }
+    return true;
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('URL', this.state.foto);
+    const dogBreed = this.state.foto.split('/')[4];
+    alert(dogBreed);
+  }
+
+  async fetchFunc() {
+    let dog = await fetch('https://dog.ceo/api/breeds/image/random');
+    dog = await dog.json();
+    const { message } = dog;
+    this.setState({
+      foto: message,
+    });
+  }
+
+  render() {
+    const { foto } = this.state;
+    const imagem = <img className="dogImg" src={ foto } alt="Foto fofa" />;
+    const showLoading = 'Loading...';
+    return (
+      <section>
+        <h1>Cachorro fofo. menos o Terrier (por algum motivo que nao sei)</h1>
+        <button type="button" onClick={ this.fetchFunc }>Mais um cachorrin</button>
+        <div>
+          { foto === '' ? showLoading : imagem }
+        </div>
+
+      </section>
+    );
+  }
 }
 
 export default App;
